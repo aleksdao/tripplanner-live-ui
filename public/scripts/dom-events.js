b046b876d39e68a88541412c43a469307e8e3ad9
distres117 <omcrobbie@gmail.com>
2016-03-12 17:55:09 -0500

    map interaction

--- public/scripts/dom-events.js
+++ public/scripts/dom-events.js
@@ -1,7 +1,24 @@
 $(function() {
   $("#selection-panel").on("click", "button", function() {
     var category = $(this).val();
-    console.log($("#" + category + "Chooser").val())
-  })
-})
+    var elem = $("#" + category + "Chooser");
+    var li = $("<li>" + name + "</li>");
+    var index = elem.prop('selectedIndex');
+    var item = places[category][Number(index)];
+    var location = {
+      lat:item.place.location[0],
+      lng: item.place.location[1]
+    };
 
+    $("#chosen-" + category).append(li);
+    setMarker(item.name, location);
+  });
+});
+
+function setMarker(name, location){
+    var marker = new google.maps.Marker({
+      title: name,
+      position: location
+    });
+    marker.setMap(map);
+}
