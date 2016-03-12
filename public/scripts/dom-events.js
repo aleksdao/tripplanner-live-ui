e4fcf15ac75d6f637dbf4b6d3e633bb4011de39d
Alex Dao <rahx1t@gmail.com>
2016-03-12 16:46:23 -0500

    w/ gitignore

--- public/scripts/dom-events.js
+++ public/scripts/dom-events.js
@@ -1,8 +1,7 @@
 $(function() {
-  console.log("hey we're in here");
   $("#selection-panel").on("click", "button", function() {
     var category = $(this).val();
-    console.log(category);
     console.log($("#" + category + "Chooser").val())
   })
 })
+
