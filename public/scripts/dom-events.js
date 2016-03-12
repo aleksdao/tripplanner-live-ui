90f86b037cdb6ea1f58656448f0ec677083ffb92
Alex Dao <rahx1t@gmail.com>
2016-03-12 18:57:57 -0500

    added/removed map markers

--- public/scripts/dom-events.js
+++ public/scripts/dom-events.js
@@ -1,24 +1,55 @@
 $(function() {
   $("#selection-panel").on("click", "button", function() {
     var category = $(this).val();
+    console.log(category);
     var elem = $("#" + category + "Chooser");
-    var li = $("<li>" + name + "</li>");
     var index = elem.prop('selectedIndex');
     var item = places[category][Number(index)];
+    var li = $("<li>" + item.name + "</li>");
+
     var location = {
       lat:item.place.location[0],
       lng: item.place.location[1]
     };
+    var chosenElem = $("#chosen-" + category);
 
-    $("#chosen-" + category).append(li);
-    setMarker(item.name, location);
+    if(category != "Hotels") {
+      chosenElem.append(li);
+    }
+    else {
+      if(chosenElem.children().length != 0) {
+        console.log("hotel present");
+        var removeName = $(chosenElem.children()[0]).html();
+        console.log(removeName);
+        removeMarker(removeName);
+      }
+      chosenElem.html(li);
+    }
+
+    setMarker(item, location, category);
   });
 });
 
-function setMarker(name, location){
+function setMarker(item, location, category){
     var marker = new google.maps.Marker({
-      title: name,
-      position: location
+      title: item.name,
+      position: location,
+      animation: google.maps.Animation.DROP
     });
+    bounds.extend(marker.getPosition());
+    // markers.push(marker);
     marker.setMap(map);
+    markers[item.name] = marker;
+    map.fitBounds(bounds);
+}
+
+function removeMarker(name) {
+  var marker = markers[name];
+  marker.setMap(null);
+  delete markers[name];
+  bounds = new google.maps.LatLngBounds();
+  for(var name in markers) {
+    bounds.extend(markers[name].getPosition());
+  }
+  map.fitBounds(bounds);
 }
