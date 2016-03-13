d4dc4124dae3d8445070804884d27cc6097d04d2
distres117 <omcrobbie@gmail.com>
2016-03-12 19:55:49 -0500

    add/remove map stuff

--- public/scripts/dom-events.js
+++ public/scripts/dom-events.js
@@ -1,11 +1,12 @@
 $(function() {
   $("#selection-panel").on("click", "button", function() {
     var category = $(this).val();
-    console.log(category);
+    //console.log(category);
     var elem = $("#" + category + "Chooser");
     var index = elem.prop('selectedIndex');
     var item = places[category][Number(index)];
-    var li = $("<li>" + item.name + "</li>");
+    var li = $('<li data-name="'+ item.name +'">' + item.name + '<button class="btn btn-danger btn-xs pull-right">X</button></li>');
+
 
     var location = {
       lat:item.place.location[0],
@@ -14,21 +15,33 @@ $(function() {
     var chosenElem = $("#chosen-" + category);
 
     if(category != "Hotels") {
-      chosenElem.append(li);
+      if (!markers[item.name]){
+        chosenElem.append(li);
+        setMarker(item, location, category);
+      }
     }
     else {
       if(chosenElem.children().length != 0) {
-        console.log("hotel present");
-        var removeName = $(chosenElem.children()[0]).html();
+        //console.log("hotel present");
+        var removeName = $(chosenElem.children()[0]).data('name');
         console.log(removeName);
         removeMarker(removeName);
       }
       chosenElem.html(li);
+      setMarker(item, location, category);
     }
 
-    setMarker(item, location, category);
+
+  });
+  $("#it_body").on("click", "button", function() {
+    var li = $(this).parent();
+    console.log(li);
+    var liName = li.data("name");
+    removeMarker(liName);
+    li.remove();
   });
 });
+
 
 function setMarker(item, location, category){
     var marker = new google.maps.Marker({
@@ -43,9 +56,11 @@ function setMarker(item, location, category){
     map.fitBounds(bounds);
 }
 
+
 function removeMarker(name) {
   var marker = markers[name];
-  marker.setMap(null);
+  if (marker)
+    marker.setMap(null);
   delete markers[name];
   bounds = new google.maps.LatLngBounds();
   for(var name in markers) {
